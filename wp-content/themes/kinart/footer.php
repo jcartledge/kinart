5931f88c7d8e4fe71d1cf96c1fbcabd17707c9f3
James Cartledge <jcartledge@gmail.com>
2016-08-20 18:31:06 +1000

    Removed unnecessary template parts.

--- wp-content/themes/kinart/footer.php
+++ wp-content/themes/kinart/footer.php
@@ -12,14 +12,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'kinart' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'kinart' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'kinart' ), 'kinart', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
