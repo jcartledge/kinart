05b76e1f2bb98accfcd19ad6596c8351c8c70082
James Cartledge <jcartledge@gmail.com>
2016-09-10 18:14:10 +1000

    Fixed z-index bug with colour animation.

--- wp-content/themes/kinart/src/js/menu-icon.js
+++ wp-content/themes/kinart/src/js/menu-icon.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  const menu_icon = document.querySelector('.menu-icon');
-  const front_page_colours = document.querySelector('.front-page-colours');
+import once from 'once-event-listener';
+
+const domReady = document.addEventListener.bind(document, 'DOMContentLoaded');
+
+domReady((e) => {
+  const $ = document.querySelector.bind(document);
+  const menu_icon = $('.menu-icon');
+  const menu = $('.menu');
+  const front_page_colours = $('.front-page-colours');
   if (menu_icon) {
     menu_icon.addEventListener('click', e => {
       menu_icon.classList.toggle('menu-icon--open');
+
+      if (front_page_colours.classList.contains('front-page-colours--open')) {
+        front_page_colours.classList.remove('front-page-colours--back');
+      } else {
+        once(front_page_colours, 'animationend', e => {
+          front_page_colours.classList.add('front-page-colours--back');
+        });
+      }
       front_page_colours.classList.toggle('front-page-colours--open');
     });
   }
