86055ac929ed850e3a37ebfa628aad58e1257da7
James Cartledge <jcartledge@gmail.com>
2016-10-07 17:25:33 +1100

    Fixed JS error.

--- wp-content/themes/kinart/src/js/category-nav.js
+++ wp-content/themes/kinart/src/js/category-nav.js
@@ -3,16 +3,18 @@ import once from 'once-event-listener';
 
 on(document, 'DOMContentLoaded', _ => {
   const categoryTitle = document.querySelector('.category-title');
-  const contentArea = document.querySelector('.content-area');
-  const widgetArea = document.querySelector('.widget-area');
+  if (categoryTitle) {
+    const contentArea = document.querySelector('.content-area');
+    const widgetArea = document.querySelector('.widget-area');
 
-  on(categoryTitle, 'click', _ => {
-    contentArea.classList.add('is-out');
-    widgetArea.classList.add('is-in');
-    once(widgetArea.querySelector('.current-menu-item'), 'click', e => {
-      e.preventDefault();
-      contentArea.classList.remove('is-out');
-      widgetArea.classList.remove('is-in');
+    on(categoryTitle, 'click', _ => {
+      contentArea.classList.add('is-out');
+      widgetArea.classList.add('is-in');
+      once(widgetArea.querySelector('.current-menu-item'), 'click', e => {
+        e.preventDefault();
+        contentArea.classList.remove('is-out');
+        widgetArea.classList.remove('is-in');
+      });
     });
-  });
+  }
 });
