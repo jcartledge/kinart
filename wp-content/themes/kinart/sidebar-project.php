a97c5d2a3c44363877dcd5409be531e1ec661e7f
James Cartledge <jcartledge@gmail.com>
2017-03-28 12:41:02 +1100

    Various CSS tweaks.

--- wp-content/themes/kinart/sidebar-project.php
+++ wp-content/themes/kinart/sidebar-project.php
@@ -5,8 +5,7 @@
 
 $category = array_pop( get_the_category() );
 ?>
-<a href="<?php echo get_category_link( $category->term_id ); ?>" class="category-title">
-  <i class="fa fa-angle-left"></i>
+<a href="/#shownav" class="category-title">
   <?php echo $category->name; ?>
 </a>
 <?php get_template_part( 'template-parts/project_list' );
