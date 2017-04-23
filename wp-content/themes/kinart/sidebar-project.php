cf0de4a83e22e0063af36819a9a090212379e605
James Cartledge <jcartledge@gmail.com>
2017-04-23 13:06:03 +1000

    Bundle of fixes.

--- wp-content/themes/kinart/sidebar-project.php
+++ wp-content/themes/kinart/sidebar-project.php
@@ -5,8 +5,6 @@
 
 $category = array_pop( get_the_category() );
 ?>
-<a href="/#shownav" class="category-title">
-  <?php echo $category->name; ?>
-</a>
+<a href="/#shownav" class="category-title"><?php echo $category->name; ?></a>
 <?php get_template_part( 'template-parts/project_list' );
 
