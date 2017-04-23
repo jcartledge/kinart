072e5e5a81ac24b10d8c285d9dcb714fa6189576
James Cartledge <jcartledge@gmail.com>
2017-04-23 14:15:53 +1000

    Switched tabs to spaces. I tried, but no.

--- wp-content/themes/kinart/src/js/colour-pairs.js
+++ wp-content/themes/kinart/src/js/colour-pairs.js
@@ -9,28 +9,28 @@ const bgAttr = 'fill';
 const interval = 5000;
 
 function insertAfter (newNode, referenceNode) {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 on(document, 'DOMContentLoaded', _ => {
-	if (window.colourPairs) {
-		const update = (colourPairIndex = 0) => {
-			const colourPair = window.colourPairs[colourPairIndex];
-			const nextIndex = (colourPairIndex + 1) % window.colourPairs.length;
-			const oldCurtain = document.querySelector(curtainSelector);
-			if (oldCurtain && !document.querySelector(curtainOpenSelector)) {
-				const newCurtain = oldCurtain.cloneNode(true);
-				const fg = newCurtain.querySelector(fgSelector);
-				const bg = newCurtain.querySelector(bgSelector);
-				fg.style[fgAttr] = colourPair.fg;
-				bg.style[bgAttr] = colourPair.bg;
-				insertAfter(newCurtain, oldCurtain);
-				on(fg, 'animationend', _ => {
-					oldCurtain && oldCurtain.parentNode && oldCurtain.parentNode.removeChild(oldCurtain);
-				});
-			}
-			window.setTimeout(_ => { update(nextIndex); }, interval);
-		};
-		window.setTimeout(update, interval);
-	}
+  if (window.colourPairs) {
+    const update = (colourPairIndex = 0) => {
+      const colourPair = window.colourPairs[colourPairIndex];
+      const nextIndex = (colourPairIndex + 1) % window.colourPairs.length;
+      const oldCurtain = document.querySelector(curtainSelector);
+      if (oldCurtain && !document.querySelector(curtainOpenSelector)) {
+        const newCurtain = oldCurtain.cloneNode(true);
+        const fg = newCurtain.querySelector(fgSelector);
+        const bg = newCurtain.querySelector(bgSelector);
+        fg.style[fgAttr] = colourPair.fg;
+        bg.style[bgAttr] = colourPair.bg;
+        insertAfter(newCurtain, oldCurtain);
+        on(fg, 'animationend', _ => {
+          oldCurtain && oldCurtain.parentNode && oldCurtain.parentNode.removeChild(oldCurtain);
+        });
+      }
+      window.setTimeout(_ => { update(nextIndex); }, interval);
+    };
+    window.setTimeout(update, interval);
+  }
 });
